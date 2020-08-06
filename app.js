ddfa593c247e8cde1a54cc56a2fd8abbe97a01b9
Prabin Parajuli <prabin.b9@gmail.com>
2020-08-06 16:04:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const people = ['Rowe', 'Prevost', 'Gare'];
 
 const peopleLis = people.map(person =>
   // expression goes here:
-
+    <li>{person}</li>));
 );
 
 // ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>,
+document.getElementById('app')
+);
