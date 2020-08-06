904c5dc76956608d45d10b8d0087eff4018c4f7f
Prabin Parajuli <prabin.b9@gmail.com>
2020-08-06 15:38:33 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+
+);
+
+// ReactDOM.render goes here:
